91d99eecb89f7a3802e6ec56dac64fdf1dd094ab
Hugo Fabricio <hugo@brzdigital.com.br>
2019-05-30 11:30:46 -0300

    Lint js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,27 +1,27 @@
 // Load jQuery from NPM
-import $ from "jquery";
+import $ from 'jquery'
 
-import fiap from "../images/fiap.svg";
+import fiap from '../images/fiap.svg'
 
-import "./modules/mask";
-import "./modules/share";
+import './modules/mask'
+import './modules/share'
 
-window.jQuery = $;
-window.$ = $;
+window.jQuery = $
+window.$ = $
 
 export default class App {
-  constructor() {
+  constructor () {
     console.log(
-      "\n%c     ",
+      '\n%c     ',
       `font-size: 30px; background: url(${
         window.location.origin
       }/${fiap}) no-repeat;`
-    );
+    )
     console.log(
-      "%cWe love to inspect codes too \ud83d\udc40\ud83d\ude42",
-      "font-size: 16px; color: #ED145B"
-    );
+      '%cWe love to inspect codes too \ud83d\udc40\ud83d\ude42',
+      'font-size: 16px; color: #ED145B'
+    )
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => new App());
+document.addEventListener('DOMContentLoaded', () => new App())
